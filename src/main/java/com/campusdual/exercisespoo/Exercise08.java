356d373b4f8207d32780e4938099375f38ab9c9d
Svinella <148323965+Svinella@users.noreply.github.com>
2023-10-26 11:19:34 +0200

    add

---
+++ src/main/java/com/campusdual/exercisespoo/Exercise08.java
@@ -0,0 +1,18 @@
+package com.campusdual.exercisespoo;
+
+import java.sql.SQLOutput;
+import java.util.Scanner;
+
+public class Exercise08 {
+
+    public static void main(String[] args) {
+
+    }
+
+    public static void mostrarSaludoConUtils() {
+        String nombreUsuario = com.campusdual.util.Utils.string("Introduce tu nombre");
+        System.out.println();
+
+
+    }
+}
---
+++ src/main/java/com/campusdual/exercisespoo/Sandbox.java
@@ -0,0 +1,22 @@
+package com.campusdual.exercisespoo;
+
+public class Sandbox {
+    public static void main(String[] args) {
+        int i = 0;
+                while(true){
+                    i++;
+                    System.out.println(i);
+                        int j = 0;
+                    while(true) {
+
+                        j++;
+                        System.out.println(j);
+                        if (j ==25){
+                            break;
+                        }
+                    }
+                }
+
+
+    }
+}
